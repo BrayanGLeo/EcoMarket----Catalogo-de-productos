78365bca3a8325ba036b9988824460b5014a7bd5
BrayanGLeo <br.godoys@duocuc.cl>
2025-06-30 10:57:28 -0400

    Ready

--- src/main/java/com/EcoMarket/Producto/model/Producto.java
+++ src/main/java/com/EcoMarket/Producto/model/Producto.java
@@ -7,13 +7,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Producto {
+@EqualsAndHashCode(callSuper = false)
+public class Producto extends RepresentationModel<Producto> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
